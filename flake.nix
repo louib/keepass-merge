cfaca5ee2d9273f8eb3dff82eb4aac8b101be837
louib <code@louib.net>
2024-01-25 22:46:09 -0500

    chore: update flake keepass-rs hash

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
                 outputHashes = {
                   # This hash need to be updated everytime you bump the version of the keepass-rs
                   # library.
-                  "keepass-0.0.0-placeholder-version" = "sha256-ltym3uvsJC8rUTqIqMhHJ04QDl4+3p9EA97HnwKOqMc=";
+                  "keepass-0.0.0-placeholder-version" = "sha256-AE1pZNvil/ZvgpKxzjNzp6rywP0oXdEBMe+QOZT+9Bo=";
                 };
               };
 
